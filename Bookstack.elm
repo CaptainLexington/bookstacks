77810e9ef18ff9efcabbddbe0ca5fcddcd1d172b
Dale <dale@chrisdale.name>
2015-06-19 06:54:22 -0500

    Closer to something like something

--- Bookstack.elm
+++ Bookstack.elm
@@ -1,8 +1,29 @@
+module Bookstack (Model, Action, update) where
+
+import Graphics.Element exposing (..)
+import Graphics.Input.Field as Field
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import List
+import String
+import StartApp
+
+
+
+
+-- main
+
+main =
+    StartApp.start { model = {title = "", entries = Nothing}, update = update, view = view }
+
+
+
 -- MODEL
 
 type alias Model = 
     { title : String,
-      entries :  List String
+      --entries :  List String
     }
 
 
@@ -15,13 +36,21 @@ type Action
 update : Action -> Model -> Model
 update action model =
   case action of
-    Create string -> String.split "\n" string
-    Pop -> 
+    Create string -> 
         { model |
-            entries <- tail model.entries
+            entries <- String.split "\n" string
         }
+    --Pop -> 
+    --    { model |
+    --        entries <- List.tail model.entries
+    --    }
 
--- VIEW
+ -- VIEW
 
-view : Model -> Html
-view =
+view : Singal.Address Action -> Model -> Html
+view address model =
+  div []
+    [ 
+      div [] [ text (toString model) ]
+    , button [ onClick address Create ] [ text "+" ]
+    ]
